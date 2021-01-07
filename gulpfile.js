b93efa7541078d24cf566fe55d6b32ec32e5b5cd
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-01-07 15:16:42 -0600

    Funciones y Video
    +Terminada la seccion de Video
    +Agregados Mixins para Grid
    +Agregadas funciones mara minificar Imagenes
    +Agregadas funciones para convertir imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+
+
+
+/* PATHS */
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss'
+
+}
 
 // Funcion que compila SASS
 
 function compilarCSS() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass({
             outputStyle: 'expanded'
         }))
@@ -13,17 +25,35 @@ function compilarCSS() {
 }
 
 function minificarCSS() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass({
             outputStyle: 'compressed'
         }))
         .pipe(dest('./build/css'))
 }
 
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(imagemin())
+        .pipe(dest('./build/img'))
+        .pipe(notify({ message: 'Imagen Minificada' }));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('./build/img'))
+        .pipe(notify({ message: 'Imagen convertida' }));
+}
+
 function watchArchivos() {
-    watch('src/scss/**/*.scss', compilarCSS);
+    watch(paths.scss, compilarCSS);
 }
 
 exports.compilarCSS = compilarCSS;
 exports.minificarCSS = minificarCSS;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+exports.versionWebp = versionWebp;
+
+exports.default = series(compilarCSS, imagenes, versionWebp, watchArchivos);
