f0356b166a6b0d40022dfdac3ecc9f62ffb80b96
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-01-08 15:47:52 -0600

    Navbar Fija
    +Agregada la funcion para fijar Navbar
    +Sitio web terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     scrollNav();
+    navegacionFija();
 });
+
+
+function navegacionFija() {
+
+    const barra = document.querySelector('.header');
+
+    //Registrar el Interseccion Observer
+    const observer = new IntersectionObserver(function(entries) {
+        if (entries[0].isIntersecting) {
+            barra.classList.remove('fijo');
+        } else {
+            barra.classList.add('fijo');
+        }
+    });
+    //Elemento a Observar
+    observer.observe(document.querySelector('.video'));
+}
 
 function scrollNav() {
     const enlaces = document.querySelectorAll('.navegacion-principal a');
