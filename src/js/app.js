f1a7da7bf56de4851771f21cc41955e5b3823e57
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-01-08 15:15:54 -0600

    ScrollView Smooth
    +Añadido desplazamiento entre secciones con efecto lento

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+});
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach(function(enlace) {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value)
+            seccion.scrollIntoView({
+                behavior: 'smooth'
+            });
+        })
+    });
+}
